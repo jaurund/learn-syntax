ec09ba15f399d2160eaf786c28a4048255362e86
jaurund <sjv042025@gmail.com>
2025-09-04 14:38:35 +0200

    First Kata from GPT done: Greet the user. Building a class with a string, with a value that I will create in Program

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// Kata 1: Greet the user
+
+/*public class Greeting
+{
+    public string Greet(string name)
+    {
+        string msg = $"Hello, {name}!";
+        return msg;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var greeting = new Greeting();
+        string message = greeting.Greet("Alice");
+
+        Console.WriteLine(message);
+    }
+}*/
+
+// Trying to rewrite the solution from memory
+
+public class Greeting
+{
+    public string Greet(string name)
+    {
+        string msg = "Hello, " + name + "!";
+        return msg;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
