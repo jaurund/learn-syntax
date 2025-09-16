b9ac3194e1482c9d5d9c793ff472f735afc8a2f8
jaurund <sjv042025@gmail.com>
2025-09-16 09:46:20 +0200

    set up a test project, and started trying some syntax for the solution

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿// https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/train/csharp
+﻿using System.Collections;
 
 public static class Kata
 {
     public static object[] RemoveEveryOther(object[] arr)
     {
-        // happy coding
+        List<T> list = new();
+        int i = 1;
+        while (i < list.Count)
+        {
+            list.RemoveAt(i);
+            i++;
+        }
     }
 }
+
+// https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/train/csharp
